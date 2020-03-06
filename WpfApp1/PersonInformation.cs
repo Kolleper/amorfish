299b397efa67d2a03032dbb55e938cfb336323c7
Kolleper <61471977+Kolleper@users.noreply.github.com>
2020-03-06 21:14:17 +0400

    New Class Person
    Создал класс для доступа полей

---
+++ WpfApp1/PersonInformation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WpfApp1
+{
+    class PersonInformation
+    {
+        public string FullName
+        {
+            get;
+            set;
+        }
+
+        public string Gender
+        {
+            get;
+            set;
+        }
+
+        public int NubmerOfPhone
+        {
+            get;
+            set;
+        }
+
+        public string HomeAdress
+        {
+            get;
+            set;
+        }
+
+        public string BirthDay
+        {
+            get;
+            set;
+        }
+    }
+}
